ba3c33ca2f7a8e4503fc65e88e273d7876b1381e
Akash00masai <rathour.akash00@gmail.com>
2025-01-07 00:59:18 +0530

    last modified

--- design.js
+++ design.js
@@ -1,1 +1,9 @@
-var s =3;
+
+function findEvenNumbers(arr)
+{
+    return arr.filter(num => num % 2 === 0);
+}
+
+
+const numbers = [1, 2, 3, 4, 5, 6];
+console.log("Even Numbers:", findEvenNumbers(numbers));
